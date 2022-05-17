69340d4c988f193f62deebe1357f6ed53c9fcf7b
NGUYỄN NHẬT TIẾN <tienduje@gmail.com>
2022-05-17 14:46:20 +0700

    Delete thongtincanhan.py

--- PYTHON/lamquencsOOPs/thongtincanhan.py
+++
@@ -1,9 +0,0 @@
-class thongtincanhan:
-    def __init__(self,hoten,lop,tuoi):
-        self.hoten=hoten;
-        self.lop=lop;
-        self.tuoi=tuoi;
-    def display(self):
-        print("hoten:%s \n lop:%s \n tuoi:%d \n"%(self.hoten,self.lop,self.tuoi))
-ttcn1=thongtincanhan("Nguyễn Nhật Tiến","CNTT15B",19)
-ttcn1.display();
