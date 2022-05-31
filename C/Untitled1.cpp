69204b21d6c2334ecd99c36a6efe8946ad093803
NGUYỄN NHẬT TIẾN <tienduje@gmail.com>
2022-05-31 08:57:16 +0700

    Add files via upload

--- C/Untitled1.cpp
+++ C/Untitled1.cpp
@@ -1,21 +1,39 @@
-#include<stdio.h>
-int main()
-{
- int i,size;
- int sum;
- printf("Nhap vao so phan tu cua mang:\n");
- scanf("%d",&size);
- int a[size];
- for(i=0;i<(size);i++){
-  printf("Nhap gia tri cho phan tu thu %d: ",i);
-  scanf("%d",&a[i]);
- }
- sum=0;
- for(i=0;i<size;i++){
-  if(a[i]%2==0){
-   sum = sum + a[i];
-  } 
- }
- printf("%d",sum);
+#include <stdio.h>
+ 
+int main(){
+    int a[100];
+    int n;
+    printf("\nNhap so luong phan tu n = ");
+    do{
+        scanf("%d", &n);
+        if(n <= 0){
+            printf("\nNhap lai n = ");
+        }
+    }while(n <= 0);
+    
+    for(int i = 0; i < n; i++){
+        printf("\nNhap a[%d] = ",i);
+        scanf("%d", &a[i]);
+    }
+    
+    // Sap xep dung thuat toan sap xep chon
+    int tg;
+    for(int i = 0; i < n - 1; i++){
+        for(int j = i + 1; j < n; j++){
+            if(a[i] > a[j]){
+                // Hoan vi 2 so a[i] va a[j]
+                tg = a[i];
+                a[i] = a[j];
+                a[j] = tg;        
+            }
+        }
+    }
+    
+    
+    printf("\nMang da sap xep la: ");
+    for(int i = 0; i < n; i++){
+        printf("%5d", a[i]);
+    }
+    
+    
 }
-
